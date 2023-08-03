d2d6e4c95d7d639b2a5c6f821d9f9512b416ccdd
ShashiRanjan1211 <shashiranjan.is20@rvce.edu.in>
2023-08-04 02:07:34 +0530

    rsa corrected

--- rsa.cpp
+++ rsa.cpp
@@ -1,36 +1,55 @@
 #include <iostream>
 #include <cmath>
+#include <bits/stdc++.h>
+#include <vector>
+
 using namespace std;
-bool isPrime(int n) {
-    if (n <= 1) {
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
         return false;
     }
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) {
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
             return false;
         }
     }
     return true;
 }
-int gcd(int a, int b) {
-    if (b == 0) {
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
         return a;
     }
     return gcd(b, a % b);
 }
-int modInverse(int a, int m) {
+
+int modInverse(int a, int m)
+{
     a = a % m;
-    for (int x = 1; x < m; x++) {
-        if ((a * x) % m == 1) {
+    for (int x = 1; x < m; x++)
+    {
+        if ((a * x) % m == 1)
+        {
             return x;
         }
     }
     return -1;
 }
-int modExp(int base, int exp, int mod) {
+
+int modExp(int base, int exp, int mod)
+{
     int result = 1;
-    while (exp > 0) {
-        if (exp % 2 == 1) {
+    while (exp > 0)
+    {
+        if (exp % 2 == 1)
+        {
             result = (result * base) % mod;
         }
         base = (base * base) % mod;
@@ -39,17 +58,20 @@ int modExp(int base, int exp, int mod) {
     return result;
 }
 
-int main() {
+int main()
+{
     int p, q, n, phi, e, d;
     cout << "Enter a prime number for p: ";
     cin >> p;
-    while (!isPrime(p)) {
+    while (!isPrime(p))
+    {
         cout << "p must be a prime number. Please enter another value for p: ";
         cin >> p;
     }
     cout << "Enter a prime number for q: ";
     cin >> q;
-    while (!isPrime(q)) {
+    while (!isPrime(q))
+    {
         cout << "q must be a prime number. Please enter another value for q: ";
         cin >> q;
     }
@@ -58,21 +80,37 @@ int main() {
     cout << "phi = " << phi << endl;
     cout << "Choose a value for e such that 1 < e < phi and gcd(e, phi) = 1: ";
     cin >> e;
-    while (e <= 1 || e >= phi || gcd(e, phi) != 1) {
-        if(e>=phi || e<=1)  {cout <<"Enter correct value of e:"<<endl;
-           cin>>e;
-           else e++;
+    while (e <= 1 || e >= phi || gcd(e, phi) != 1)
+    {
+        cout << "Enter correct value of e:" << endl;
+        cin >> e;
     }
     d = modInverse(e, phi);
     cout << "Public key (e, n): (" << e << ", " << n << ")" << endl;
     cout << "Private key (d, n): (" << d << ", " << n << ")" << endl;
-    int plaintext;
+
+    string plaintext;
     cout << "Enter the plaintext to be encrypted: ";
-    cin >> plaintext;
-    int ciphertext = fmod(pow(plaintext, e), n);
-    cout << "Encrypted message: " << ciphertext << endl;
-plaintext = modExp(ciphertext, d, n);
-    cout << "Decrypted message: " << plaintext << endl;
+    cin.ignore();
+    getline(cin, plaintext);
+    cout << "Encrypted message: ";
+    vector<int> encryptedText;
+    for (char ch : plaintext)
+    {
+        int charValue = static_cast<int>(ch) - 'a';
+        int encryptedCharValue = modExp(charValue, e, n);
+        encryptedText.push_back(encryptedCharValue);
+        cout << encryptedCharValue << " ";
+    }
+    cout << "Decrypted message: ";
+    string decryptedText;
+    for (size_t i = 0; i < encryptedText.size(); i++)
+    {
+        int decryptedCharValue = modExp(encryptedText[i], d, n);
+        decryptedCharValue = (decryptedCharValue + 26) % 26;
+        decryptedCharValue = (decryptedCharValue + 'a');
+        decryptedText += static_cast<char>(decryptedCharValue);
+    }
+    cout << decryptedText << endl;
     return 0;
 }
-
